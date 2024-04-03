ad4dd411cfb53f93e06847e0101bcc078cc5c866
Backdraft2100 <edwin.lanckbeen@gmail.com>
2024-04-03 21:31:40 +0200

    Add marten for postgress document db

--- scr/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
+++ scr/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -1,7 +1,4 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-namespace Catalog.API.Products.CreateProduct;
+﻿namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(
     string Name,
@@ -11,7 +8,7 @@ public record CreateProductCommand(
     decimal Price) : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler
+internal class CreateProductCommandHandler(IDocumentSession session)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -32,8 +29,10 @@ internal class CreateProductCommandHandler
         };
 
         // save to database
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
 
         // return result
-        return new CreateProductResult(Guid.NewGuid());
+        return new CreateProductResult(product.Id);
     }
 }
