d4279334bdf071df0e3db06c057af801a75b5087
Backdraft2100 <edwin.lanckbeen@gmail.com>
2024-04-09 08:58:40 +0200

    Get by category

---
+++ scr/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Catalog.API.GetProductsByCategory;
+
+// public record GetProductByCategoryRequest(string Category) : IQuery<GetProductByCategoryRequest>;
+
+public record GetProductsByCategoryResponse(IEnumerable<Product> Products);
+
+public class GetProductByCategoryEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/category/{category}", async (string Category, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByCategoryQuery(Category));
+
+            var response = result.Adapt<GetProductsByCategoryResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProductByCategory")
+        .Produces<GetProductsByCategoryResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get product by category")
+        .WithDescription("Get products by category");
+    }
+}
