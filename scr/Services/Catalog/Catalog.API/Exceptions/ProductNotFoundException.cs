47900fd3ff6decce3a54355fdab88e912f6e48b3
Backdraft2100 <edwin.lanckbeen@gmail.com>
2024-04-09 22:01:57 +0200

    Add global exceptio handler

--- scr/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
+++ scr/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -1,7 +1,9 @@
+using BuildingBlocks.Exceptions;
+
 namespace Catalog.API.Exceptions;
 
 
-internal class ProductNotFoundException : Exception
+internal class ProductNotFoundException : NotFoundException
 {
-    public ProductNotFoundException() : base("Product not found!") { }
+    public ProductNotFoundException(Guid Id) : base("Product", Id) { }
 }
