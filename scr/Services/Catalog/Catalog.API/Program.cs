7332209ae26e1bd69621f384cd1db82e90207264
Backdraft2100 <edwin.lanckbeen@gmail.com>
2024-04-09 16:07:04 +0200

    Register validtion behavior in Catalog.API

--- scr/Services/Catalog/Catalog.API/Program.cs
+++ scr/Services/Catalog/Catalog.API/Program.cs
@@ -1,13 +1,17 @@
+using BuildingBlocks.Behavior;
 using Microsoft.CodeAnalysis.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Populate DI Container
-builder.Services.AddCarter();
+var assembly = typeof(Program).Assembly;
 builder.Services.AddMediatR(config =>
 {
-    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+    config.RegisterServicesFromAssemblies(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
+builder.Services.AddValidatorsFromAssembly(assembly);
+builder.Services.AddCarter();
 builder.Services.AddMarten(opt =>
 {
     opt.Connection(builder.Configuration.GetConnectionString("Database")!);
