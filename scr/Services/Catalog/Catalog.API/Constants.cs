3eabdbfbf2656112cc1c3e68a2dbbb7145b3a914
Backdraft2100 <edwin.lanckbeen@gmail.com>
2024-04-09 09:43:16 +0200

    Catalog.API constants

---
+++ scr/Services/Catalog/Catalog.API/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.API;
+
+public static class Constants
+{
+    public static class Routes
+    {
+        public const string GetProductsById = "/products/{id}";
+        public const string GetProductsByCategory = "/products/category/{category}";
+        public const string UpdateProduct = "/products";
+    }
+}
