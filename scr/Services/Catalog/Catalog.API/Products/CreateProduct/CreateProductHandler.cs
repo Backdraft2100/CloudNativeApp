efda47a3e24581effb3f712c51c2c4a5c96fb148
Backdraft2100 <edwin.lanckbeen@gmail.com>
2024-04-09 22:04:49 +0200

    Add centralised logging behavior

--- scr/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ scr/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -20,12 +20,11 @@ public class CreateProductCommandValidator : AbstractValidator<CreateProductComm
 }
 
 internal class CreateProductCommandHandler
-    (IDocumentSession session, ILogger<CreateProductCommandHandler> logger)
+    (IDocumentSession session)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        logger.LogInformation("CreateProductCommandHandler.Handle called with {@command}", command);
         // Business logic
         // Create Product entity from command object
         // save to database
--- scr/Services/Catalog/Catalog.API/Program.cs
+++ scr/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,3 @@
-using BuildingBlocks.Behavior;
-using Microsoft.CodeAnalysis.Options;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Populate DI Container
@@ -8,6 +5,7 @@ var assembly = typeof(Program).Assembly;
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssemblies(assembly);
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
 builder.Services.AddValidatorsFromAssembly(assembly);
@@ -17,9 +15,13 @@ builder.Services.AddMarten(opt =>
     opt.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
 
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
 var app = builder.Build();
 
 // Configure HTTP request pipeline
 app.MapCarter();
+
+app.UseExceptionHandler(options => { });
 
 app.Run();
