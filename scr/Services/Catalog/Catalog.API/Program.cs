8082f13a438199e7b119260c7054938c09a329fd
Backdraft2100 <edwin.lanckbeen@gmail.com>
2024-04-10 22:25:24 +0200

    Add healtchecks

--- scr/Services/Catalog/Catalog.API/Program.cs
+++ scr/Services/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,6 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Populate DI Container
@@ -20,11 +23,20 @@ if (builder.Environment.IsDevelopment())
 
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+
 var app = builder.Build();
 
 // Configure HTTP request pipeline
 app.MapCarter();
 
 app.UseExceptionHandler(options => { });
+
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
