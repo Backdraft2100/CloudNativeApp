9a4b3f1a72b3919cb27927b319fb9b67097c5e3c
Backdraft2100 <1@1.1>
2024-04-23 13:24:35 +0200

    Add discount service

---
+++ scr/Services/Discount/Discount.Grpc/Data/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using Discount.Grpc.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc;
+
+public static class Extensions
+{
+    public static IApplicationBuilder UseMigration(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        using var dbContext = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+        dbContext.Database.MigrateAsync();
+        return app;
+    }
+
+}
