881fc404c4250a2768ee427c4bbc3013acdb9f92
Backdraft <1.1@1.1>
2024-05-25 23:25:01 +0200

    Setup Ordering CA layers, relations and DI

--- scr/Services/Ordering/Ordering.API/Program.cs
+++ scr/Services/Ordering/Ordering.API/Program.cs
@@ -1,7 +1,19 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .RegisterInfrastructureServices(builder.Configuration)
+    .RegisterApplicationServices()
+    .RegisterApiServices();
+
 
 // Populate DI Container
 var app = builder.Build();
+
+app.UseApiServices();
 
 
 // Configure HTTP request pipeline
