509d689b880f885625e8e829cc6ad9259262c6af
Backdraft <1.1@1.1>
2024-05-26 17:23:21 +0200

    Ordering Domain Models beginning

---
+++ scr/Services/Ordering/Ordering.Domain/Enums/OrderStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Ordering.Domain.Enums;
+
+public enum OrderStatus
+{
+    Draft = 100,
+    Pending = 200,
+    Completed=300,
+    Cancelled=400
+}
---
+++ scr/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -0,0 +1,18 @@
+namespace Ordering.Domain.Models;
+
+public class OrderItem : Entity<Guid>
+{
+    public OrderItem(Guid orderId, Guid producId, int quantity, decimal price)
+    {
+        OrderId = orderId;
+        ProducId = producId;
+        Quantity = quantity;
+        Price = price;
+    }
+
+
+    public Guid OrderId { get; private set;} =default!;
+    public Guid ProducId { get; private set; } =default!;
+    public int Quantity { get; private set; } =default!;
+    public decimal Price { get; private set; } =default!;
+}
