6d755dc287bce7dbad3894a0991c0e8e40d78670
Backdraft <1.1@1.1>
2024-05-26 21:48:26 +0200

    Rich domain value objects

---
+++ scr/Services/Ordering/Ordering.Domain/DomainExceptions/DomainException.cs
@@ -0,0 +1,8 @@
+﻿namespace Ordering.Domain.DomainExceptions;
+
+public class DomainException : Exception
+{
+    public DomainException(string message) 
+        : base($"Domain exception : {message} throws from domain layer.")
+    {}
+}
--- scr/Services/Ordering/Ordering.Domain/Models/Customer.cs
+++ scr/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -5,4 +5,17 @@ public class Customer : Entity<CustomerId>
     public string Name { get; private set; } =default!;
     public string Email { get; private set; } = default!;
 
+    public static Customer create(CustomerId customerId, string name, string email)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentException.ThrowIfNullOrWhiteSpace(email);
+        Customer customer = new Customer
+        {
+            Name = name,
+            Email = email
+        };
+
+        return customer;
+    }
+
 }
